2979f2352d908f6994069bee4c5ee29b2c19a6e0
Rajat Modi <rajatmodi62@gmail.com>
2020-06-04 13:08:33 +0530

    Add Forward pass of the segression layeer

--- model/segression.py
+++ model/segression.py
@@ -41,7 +41,8 @@ class Segression(nn.Module):
 
         #store parameters.
         self.epsilon= epsilon
-
+        self.in_channels= in_channels
+        self.out_channels= out_channels
         ########################################################################
         #initialize backbone
         if backbone is 'VGG':
@@ -53,7 +54,7 @@ class Segression(nn.Module):
                                       out_channels=out_channels,\
                                       pretrained= pretrained)
         else:
-            raise Exception("Backbone input not valid")
+            raise Exception("Backbone input should be VGG or ResNest")
 
         ########################################################################
         #initialize segmentation_head
@@ -66,14 +67,18 @@ class Segression(nn.Module):
         #initialize prediction heads
         #Note: in_channel=out_channel since it's plugged in front of backbone.
         if segression_dimension==1:
+            print("prediction head 1D")
+
             self.prediction_head= PredictionHead1D(segmentation_map_threshold= center_line_segmentation_threshold,\
                                                    gaussian_segmentation_threshold= gaussian_segmentation_threshold,\
                                                    epsilon=epsilon)
         elif segression_dimension==2:
+            print("prediction head 2d")
             self.prediction_head= PredictionHead2D(segmentation_map_threshold= center_line_segmentation_threshold,\
                                                    gaussian_segmentation_threshold= gaussian_segmentation_threshold,\
                                                    epsilon=epsilon)
         elif segression_dimension==3:
+            print("prediction head 3d")
             self.prediction_head= PredictionHead3D(segmentation_map_threshold= center_line_segmentation_threshold,\
                                                    gaussian_segmentation_threshold= gaussian_segmentation_threshold,\
                                                    epsilon=epsilon)
@@ -86,5 +91,44 @@ class Segression(nn.Module):
 
         print("Segression module initialized")
 
+    '''
+    Forward Pass for segression
+    Input:
+            X: image tensor [B,in_channel,H,W]
+            Segmentation Map: [B,1,H,W] (non thresholded)
+    Gaussians are drawn using segmentation map
+    '''
+    def forward(self,x,segmentation_map=None):
+        ########################################################################
+        #check input dimensions
+        assert x.size()[1]==self.in_channels,\
+            "input image channels does not match in_channels"
+        assert x.size()[2]%32==0,\
+            "Height dimension should be a multiple of 32"
+        assert x.size()[3]%32==0,\
+            "Width dimension should be a multiple of 32"
+        ########################################################################
+
+        #make a pass through backbone
+        x= self.backbone(x)
+
+        #compute variance map
+        variance= self.variance_conv(x)
+
+        #compute center line segmentation
+        center_line_segmentation= self.segmentation_head(x)
+
+        # if no external map given, use center_line_segmentation to compute gaussians.
+        if segmentation_map is None:
+            gaussian_segmentation= self.prediction_head(variance_map= variance,\
+                                                        segmentation_map= center_line_segmentation)
+        else:
+            #use externally provided map for training
+            gaussian_segmentation= self.prediction_head(variance_map=variance,\
+                                                        segmentation_map=segmentation_map)
+        #print("backbone pass",x.size())
+        return gaussian_segmentation, center_line_segmentation
 if __name__ == '__main__':
-    segression= Segression()
+    segression= Segression(backbone= "ResNest",segression_dimension=1)
+    x= torch.randn(2,3,256,512)
+    o= segression(x)
