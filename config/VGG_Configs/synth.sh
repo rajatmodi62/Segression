63445ef5f2f421a62e412d452acb9c3c2da14eb2
R. Modi <rajatmodi62@gmail.com>
2020-06-05 00:12:55 +0530

    refine CUDA devices

--- config/VGG_Configs/synth.sh
+++ config/VGG_Configs/synth.sh
@@ -12,7 +12,7 @@ UPDATE_VISDOM_ITER=100
 BACKBONE='VGG'
 #CHECKPOINT_PATH='snapshots/CTW1500_3d_rotated_gaussian_attention_'$CHECKPOINT_NO'.pth'
 ITERATION_TO_START_FROM=`expr $CHECKPOINT_NO + 1`
-CUDA_DEVICE=0
+CUDA_DEVICE=1
 nvidia-smi | grep 'python' | awk '{ print $3 }' | xargs -n1 kill -9
 
 CUDA_VISIBLE_DEVICES=$CUDA_DEVICE python train_modular.py --batch-size=$BATCH_SIZE \
